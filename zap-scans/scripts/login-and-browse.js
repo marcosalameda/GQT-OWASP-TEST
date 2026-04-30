94f2a6dba6b9dd4686ae29046a54e60d8191f1bd
marcosalameda <marcos.alameda@quidgest.pt>
2026-04-30 17:14:14 +0100

    Update login-and-browse.js

--- zap-scans/scripts/login-and-browse.js
+++ zap-scans/scripts/login-and-browse.js
@@ -69,7 +69,7 @@ const { chromium } = require('playwright');
     '/home',
     '/list'
     // add more routes whenever needed:
-    // '/details/1',
+    /#/en-US/0/GQT/menu/GQT_111
     // '/reports',
     // '/admin'
   ];
